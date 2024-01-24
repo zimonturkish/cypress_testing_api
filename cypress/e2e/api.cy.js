64c3fc34261572727932a23c8e359cfa04aa6d48
zimonturkish <simoturco85@gmail.com>
2024-01-24 18:54:26 +0100

    Test of a request to a REST API and logs of some data

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
+/// <reference types="cypress" />
+
+describe('Learning REST API Testing with Cypress', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    // request to REST API asking for response
+    cy.request('/users/2').then((response) =>{
+      // log the email of the data object
+      cy.log(JSON.stringify(response.body.data.email))
+      // log the complete headers of the response objcet
+      cy.log(JSON.stringify(response.headers))
+    })
   })
 })
