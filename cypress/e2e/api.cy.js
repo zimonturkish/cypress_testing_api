d55db71d775e775c3acbf9da2754a3e2204a35a6
zimonturkish <simoturco85@gmail.com>
2024-01-25 11:58:51 +0100

    Verifiying the status code

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -25,4 +25,14 @@ describe('Learning REST API Testing with Cypress', () => {
         .its('connection')
         .should('include', 'keep-alive')
       })
+
+      // Verifying the status code (200) for the response
+      it('API Tests - Status Codes', () =>{
+        cy.request('/users/2').as('existingUser')
+        // verify the successfull status code
+        cy.get('@existingUser').its('status').should('equal', 200)
+
+        cy.request({ url: '/users/non-exist', failOnStatusCode: false}).as('nonExistingUser')
+        cy.get('@nonExistingUser').its('status').should('equal', 404)
+      })
 })
