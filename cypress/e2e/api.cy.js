9736dcd5dca6d0966a3a6b97ed15642eb1f60b64
zimonturkish <simoturco85@gmail.com>
2024-01-25 10:55:06 +0100

    Headers validation

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -10,4 +10,19 @@ describe('Learning REST API Testing with Cypress', () => {
       cy.log(JSON.stringify(response.headers))
     })
   })
+
+  // how to validate headers
+  it('API Test - Validate Headers', () => {
+        cy.request('/users/2').as('user')
+        // the response has stored as @user
+        cy.get('@user')
+        .its('headers')
+        .its('content-type')
+        .should('include', 'application/json')
+
+        cy.get('@user')
+        .its('headers')
+        .its('connection')
+        .should('include', 'keep-alive')
+      })
 })
